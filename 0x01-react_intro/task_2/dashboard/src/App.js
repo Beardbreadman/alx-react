e5236e39a7339267c77b3808a810e4f42b4fe04e
Wonderful Emmanuel Adebayo <adebayowondeful@gmail.com>
2023-11-12 23:10:43 +0100

    intro to react

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import logo from './holberton-logo.jpg';
+import { getFooterCopy, getFullYear } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" />
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" />
+          <button type="button">OK</button>
+        </form>
+      </div>
+
+      <footer className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy()}
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
